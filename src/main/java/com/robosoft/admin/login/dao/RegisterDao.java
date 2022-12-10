86b51213d20353e10362aed57fb31cd27495a7b1
sharathrkumar <sharath.rkumar@robosoftin.com>
2022-12-10 14:50:45 +0530

    registration

---
+++ src/main/java/com/robosoft/admin/login/dao/RegisterDao.java
@@ -0,0 +1,10 @@
+package com.robosoft.admin.login.dao;
+
+import com.robosoft.admin.login.model.Register;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterDao {
+    public void adminRegister(Register register) {
+    }
+}
